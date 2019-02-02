0df37157f8978fff5ed4d51333f1821da706c9a8
Stephen Bourke <steveberky@hotmail.com>
2019-02-02 13:13:23 +0000

    TagHelpers

---
+++ WebApplication1/pages/TagHelpPlay.cshtml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.pages
+{
+    public class TagHelpPlayModel : PageModel
+    {
+        [BindProperty]
+        public int Number1 { get; set; }
+
+        [BindProperty]
+        public int? Number2 { get; set; }
+
+        [BindProperty]
+        public double Number3 { get; set; }
+
+        [BindProperty]
+        public double? Number4 { get; set; }
+
+        [BindProperty]
+        public decimal Number5 { get; set; }
+
+        [BindProperty]
+        public decimal? Number6 { get; set; }
+
+        [BindProperty]
+        public float Number7 { get; set; }
+
+        [BindProperty]
+        public float? Number8 { get; set; }
+
+        [BindProperty]
+        public string String1 { get; set; }
+
+        [BindProperty]
+        public DateTime Time1 { get; set; }
+
+        [BindProperty]
+        public DateTime? Time2 { get; set; }
+
+        [BindProperty]
+        public bool IsContactAllowed { get; set; }
+
+        [BindProperty]
+        public string Season{ get; set; }
+
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            
+        }
+
+        public void OnPost()
+        {
+            Message = String.Format("Your choice of season is {0}", Season);
+        }
+
+    }
+}
