314adf264674bc1413ec309d7281c4526fb95752
Stephen Bourke <steveberky@hotmail.com>
2019-02-19 19:58:34 +0000

    Time added on...

---
+++ WebApplication1/pages/Time.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace WebApplication1.pages
+{
+    public class TimeModel : PageModel
+    {
+        public string[] Days { get; set; } = { "Monday", "Tuesday", "Wednesday",
+        "Thursday", "Friday", "Saturday", "Sunday"};
+
+
+
+        [BindProperty]
+        public bool[] SelectedDay { get; set; } = { false, false, false, false,
+        false, false, false };
+
+
+
+        public void OnGet()
+        {
+
+        }
+
+        
+
+
+    }
+}
